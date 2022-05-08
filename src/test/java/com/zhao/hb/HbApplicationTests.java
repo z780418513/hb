aa0ca4b0b57d511b2c3350f1d30b8911d0f09776
Administrator <admin@example.com>
2022-05-08 11:03:32 +0800

    init

---
+++ src/test/java/com/zhao/hb/HbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhao.hb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
